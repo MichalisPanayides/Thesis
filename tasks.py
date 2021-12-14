420f6c669f6a87ca930701e1fcd7d819f611c5f4
Michalis Panayides <PanayidesM@cardiff.ac.uk>
2021-12-14 17:54:17 +0000

    Setup tests and checks (#1)
    * Create main.tex
    
    - main.tex in the main latex file to be ran
    - packages.tex is the file that will list all necessary packages
    - title.tex is the file for the title page of the thesis
    
    * Use the invoke library to automate checks
    
    First task is to check that the pdf compiles
    
    * Add a spellcheck task in tasks.py
    
    Uses aspell to check any typos in the writing
    
    * Add proselint task to tasks.py
    
    Use the proselint library to check the prose of the writing
    
    * Add alex as a task to tasks.py
    
    Alex is a tool that catches insensitive and inconsiderate writing
    
    * Add a doctest task to tasks.py
    
    This task is for running any python code in the latex source files
    and make sure that outputs are as expected
    
    * Create environment.yml file for thesis environment
    
    * Add continuous integration file
    
    ci.yml runs all tasks defined in tasks.py
    
    * Remove problematic and unnecessary library
    
    The CI on github failed to install ipykernel because it is
    not recognised by ubuntu.
    To properly install it this needs to be ran:
    `sudo apt-get install -y python3-ipykernel`
    Since the library was not necessary though it is removed
    completely.
    
    * Add my name to known words
    
    * Remove doctests for now
    
    Doctests output an error because no tests are discovred.
    Therefore the doctests are removed until they are needed
    
    * Remove doctests from CI as well

---
+++ tasks.py
@@ -0,0 +1,94 @@
+import pathlib
+import re
+import subprocess
+import sys
+
+from invoke import task
+import proselint as plnt
+
+import known
+
+
+@task
+def compile(c):
+    """
+    Compile the LaTeX document.
+    """
+    c.run(f"latexmk -xelatex main.tex")
+
+
+@task
+def spellcheck(c):
+    """
+    Check the spelling of all .tex files
+    """
+    all_tex_files = list(pathlib.Path().glob("**/*.tex"))
+    exit_codes = [0]
+    for path in all_tex_files:
+        latex = path.read_text()
+        aspell_output = subprocess.check_output(
+            ["aspell", "-t", "--list", "--lang=en_GB"], input=latex, text=True
+        )
+        errors = set(aspell_output.split("\n")) - {""}
+        incorrect_words = set()
+        for error in errors:
+            if not any(
+                re.fullmatch(word.lower(), error.lower()) for word in known.words
+            ):
+                incorrect_words.add(error)
+
+        if len(incorrect_words) > 0:
+            print(f"In {path} the following words are not known: ")
+            for string in sorted(incorrect_words):
+                print(string)
+            exit_codes.append(1)
+    sys.exit(max(exit_codes))
+
+
+@task
+def proselint(c):
+    """
+    Check the spelling of all .tex files
+    """
+
+    def remove_expected_errors(errors):
+        """
+        Remove errors that were expected from the error list
+        """
+        expected_errors = (
+            "annotations.misc",
+            "leonard.exclamation.30ppm",
+            "typography.symbols.ellipsis",
+            "typography.symbols.sentence_spacing",
+            "security.credit_card",
+        )
+        updated_errors = []
+        for error in errors:
+            if error[0] not in expected_errors:
+                updated_errors.append(error)
+        return updated_errors
+
+    all_tex_files = list(pathlib.Path().glob("**/*.tex"))
+    exit_codes = [0]
+    for path in all_tex_files:
+        with open(path, "r") as f:
+            text = f.read()
+            errors = plnt.tools.lint(text)
+        errors = remove_expected_errors(errors)
+        if errors:
+            print(f"In {path} the following errors were found: ")
+            for error in errors:
+                print(error)
+            exit_codes.append(1)
+    sys.exit(max(exit_codes))
+
+
+@task
+def alex(c):
+    """
+    Check for inconsiderate and insensitive writing of all .tex files
+    """
+    all_tex_files = list(pathlib.Path().glob("**/*.tex"))
+    for file in all_tex_files:
+        c.run(f"alex {file}")
+
