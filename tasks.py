4aa0979afcbc784af536d595bf509fa1c55bebdb
Michalis <michalis.pan1@hotmail.com>
2023-03-11 22:04:53 +0000

    Apply suggestions by alex, aspell and proselint

--- tasks.py
+++ tasks.py
@@ -23,26 +23,28 @@ def spellcheck(c):
     """
     Check the spelling of all .tex files
     """
+    exception_files = ["packages.tex", "Frontmatter/acknowledgements.tex"]
     all_tex_files = list(pathlib.Path().glob("**/*.tex"))
     exit_codes = [0]
     for path in all_tex_files:
-        latex = path.read_text()
-        aspell_output = subprocess.check_output(
-            ["aspell", "-t", "--list", "--lang=en_GB"], input=latex, text=True
-        )
-        errors = set(aspell_output.split("\n")) - {""}
-        incorrect_words = set()
-        for error in errors:
-            if not any(
-                re.fullmatch(word.lower(), error.lower()) for word in known.words
-            ):
-                incorrect_words.add(error)
+        if str(path) not in exception_files:
+            latex = path.read_text()
+            aspell_output = subprocess.check_output(
+                ["aspell", "-t", "--list", "--lang=en_GB"], input=latex, text=True
+            )
+            errors = set(aspell_output.split("\n")) - {""}
+            incorrect_words = set()
+            for error in errors:
+                if not any(
+                    re.fullmatch(word.lower(), error.lower()) for word in known.words
+                ):
+                    incorrect_words.add(error)
 
-        if len(incorrect_words) > 0:
-            print(f"In {path} the following words are not known: ")
-            for string in sorted(incorrect_words):
-                print(string)
-            exit_codes.append(1)
+            if len(incorrect_words) > 0:
+                print(f"In {path} the following words are not known: ")
+                for string in sorted(incorrect_words):
+                    print(string)
+                exit_codes.append(1)
     sys.exit(max(exit_codes))
 
 
